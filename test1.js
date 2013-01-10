454f9512a01d85263e3a7fbe487d8275b228d578
Jure Mav <jure.mav@gmail.com>
2013-01-10 09:51:10 +0100

    Added resortsIndex API

--- test1.js
+++ test1.js
@@ -4,6 +4,7 @@
 var config = require('./config');
 var mysql = require('mysql');
 var express = require('express');
+var async = require('async');
 var connection = mysql.createConnection(config.mysqlConn);
 
 var app = express();
@@ -13,9 +14,42 @@ var app = express();
 app.get('/countries', function(req, res) {
 	get_countries(res);
 });
+app.get('/resortsIndex', function(req, res) {
+	getResortsIndex(res);
+});
 
 
 //METHODS
+
+function getResortsIndex(res){
+	//http://www.mysqltutorial.org/stored-procedures-parameters.aspx
+	//http://stackoverflow.com/questions/10546956/is-there-a-driver-for-mysql-on-nodejs-that-supports-stored-procedures
+	var resortsQuery = 'SELECT ID as id, IDCountry as c_id, title as name FROM resorts WHERE visible = "True"';
+	connection.query(resortsQuery, function(err, resorts, fields) {
+		if (err) throw err;
+
+		//http://stackoverflow.com/questions/7653080/adding-to-an-array-asynchronously-in-node-js
+		async.forEach(resorts, function (resort, callback){
+			//console.log(resort); // print the key
+
+			var resortIndexQuery = 'CALL resort_index('+resort.id+')';
+			connection.query(resortIndexQuery, function(err, rows, fields) {
+				if (err) throw err;
+
+				rows[0].forEach(function(val) {
+					resort[val.field] = val.val;
+				});
+				callback(); // tell async that the iterator has completed
+
+			});
+
+		}, function(err) {
+			//console.log(resorts);
+			outJSON(res, resorts);
+		});
+
+	});
+}
 
 function get_countries(res) {
 //db connect
@@ -35,8 +69,9 @@ function outJSON (res, data) {
 
 
 //start app
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(3001);
+console.log('Listening on port 3001');
+//getResortsIndex();
 
 //END -> ctrl + c exit event
 process.on( 'SIGINT', function() {
@@ -66,3 +101,6 @@ process.on( 'SIGINT', function() {
 // * Every method you invoke on a connection is queued and executed in sequence.
 // * Closing the connection is done using `end()` which makes sure all remaining
 //   queries are executed before sending a quit packet to the mysql server.
+
+//MYSQL
+//http://stackoverflow.com/questions/2281890/can-i-create-view-with-parameter-in-mysql
