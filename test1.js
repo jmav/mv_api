ccc340ab1b21719f83e23eaf93e33953d5dc52aa
Jure Mav <jure.mav@gmail.com>
2013-01-18 20:27:35 +0100

    Added underscore 1.4.3 Included green slope, url index, images

--- test1.js
+++ test1.js
@@ -5,9 +5,15 @@ var config = require('./config');
 var mysql = require('mysql');
 var express = require('express');
 var async = require('async');
+var _ = require('underscore');
+var fs = require('fs');
 var connection = mysql.createConnection(config.mysqlConn);
 
+var baseImageUrl = "/Users/juremav/DEV/MountVacation/skiresorts images/sm/";
+
 var app = express();
+
+var aa;
 
 	connection.connect();
 // get_countries();
@@ -22,9 +28,14 @@ app.get('/resortsIndex', function(req, res) {
 //METHODS
 
 function getResortsIndex(res){
+
 	//http://www.mysqltutorial.org/stored-procedures-parameters.aspx
 	//http://stackoverflow.com/questions/10546956/is-there-a-driver-for-mysql-on-nodejs-that-supports-stored-procedures
-	var resortsQuery = 'SELECT ID as id, IDCountry as cid, title FROM resorts WHERE visible = "True"';
+	// var resortsQuery = 'SELECT ID as id, IDCountry as cid, title FROM resorts WHERE visible = "True"';
+	var resortsQuery = 'SELECT resorts.ID as id, IDCountry as cid, resorts.title as title, indexes.index as idx '+
+	'FROM resorts INNER JOIN indexes ON resorts.ID = indexes.tableID '+
+	'WHERE resorts.visible = "True" AND indexes.table = "resorts" GROUP BY resorts.ID';
+
 	connection.query(resortsQuery, function(err, resorts, fields) {
 		if (err) throw err;
 
@@ -36,7 +47,7 @@ function getResortsIndex(res){
 			connection.query(resortIndexQuery, function(err, rows, fields) {
 				if (err) throw err;
 
-				rows[0].forEach(function(val) {
+				rows[0].forEach(function(val, idx) {
 					if(val.val === 'True') {
 						resort[val.field] = true;
 					} else if(val.field === 'family' || val.field === 's_park' || val.field === 'seg' || val.field === 'other'){
@@ -47,6 +58,18 @@ function getResortsIndex(res){
 						} else {
 							resort[val.field] = [];
 						}
+					}else if (val.field === 'img'){
+
+						var strToArr = eval(val.val)[0] || '';
+						fs.exists(baseImageUrl + "resort_"+resort.id+"_1_sm.jpg", function(exists) { //check for maps
+							if (exists) {
+								resort[val.field] = 'map';
+							} else {
+								resort[val.field] = strToArr || 'no-image';
+								// resort[val.field] = _.first(strToArr.match(/[\d]+$/)) || 'no-image'; //just image id
+							}
+						});
+
 					} else {
 						var valN = val.val.replace(',', '.');
 						if(isNaN(valN)){
@@ -95,13 +118,14 @@ function outJS (res, data, varName) {
 app.listen(3001);
 console.log('Listening on port 3001');
 console.log('demo at: http://localhost:3001/resortsIndex');
+console.log("BE aware: images are checked at: " + baseImageUrl);
 //getResortsIndex();
 
 //END -> ctrl + c exit event
 process.on( 'SIGINT', function() {
 	connection.end(); //end conn. to db
-	console.log( "\ngracefully shutting down from  SIGINT (Crtl-C)" )
-	process.exit()
+	console.log( "\ngracefully shutting down from  SIGINT (Crtl-C)" );
+	process.exit();
 })
 
 //SAMPLES
