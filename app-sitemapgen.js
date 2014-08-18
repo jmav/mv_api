36f36246bc7d17e769d093ca9b394149eab9c78a
Jure Mav <jure.mav@gmail.com>
2014-08-18 13:50:30 +0200

    add: support for sitemap "active" code clean-up

--- app-sitemapgen.js
+++ app-sitemapgen.js
@@ -1,95 +1,98 @@
 //generate sitemap.xml from active DB & copy it to server
 var config = require('./config'),
-	sm = require('./sitemapgen.js'),
-	Q = require('q'),
-	// express = require('express'),
-	mysql = require('mysql'),
-	_ = require('underscore'),
-	pd = require('pretty-data').pd,
-	fs = require('fs'),
-	sftp = require('sftp'),
-	log4js = require('log4js');
+    sm = require('./sitemapgen.js'),
+    Q = require('q'),
+    // express = require('express'),
+    mysql = require('mysql'),
+    _ = require('underscore'),
+    pd = require('pretty-data').pd,
+    fs = require('fs'),
+    sftp = require('sftp'),
+    log4js = require('log4js');
 
 //dev modules
 var inspect = require('eyes').inspector();
 
 // init
 var logger = log4js.getLogger(),
-	dbConProd1 = mysql.createConnection(config.mysqlConn_1),
-	domains = require('./config/domains.json').production,
-	domainsQ = [];
+    dbConProd1 = mysql.createConnection(config.mysqlConn_1),
+    domains = require('./config/domains.json').production,
+    domainsQ = [];
 
 domains = _.omit(domains, ['default']);
 
 // get urls from DB
 var getUrls = function(options) {
 
-	if ( !options.lang ) { return; }
+    if (!options.lang) {
+        return;
+    }
 
-	var deferred = Q.defer();
+    var deferred = Q.defer();
 
-	config.sftp_api1.home = config.pathMap['live_sitemap'] + options.lang; //set sftp path
+    config.sftp_api1.home = config.pathMap['live_sitemap'] + options.lang; //set sftp path
 
-	var queryStr =	'SELECT url, ' +
-					'CASE  ' +
-					'    WHEN path LIKE "accommodation.%" THEN 0.8 ' +
-					'    WHEN path LIKE "resort.%" THEN 0.9 ' +
-					'    WHEN path LIKE "resortfinder.%" THEN 0.7 ' +
-					'    else 5 ' +
-					'END as priority ' +
-					'FROM routing  ' +
-					'WHERE lang = ? ' +
-					'AND redirect = 0 ' +
-					'ORDER BY url ';
+    var queryStr = 'SELECT url, ' +
+        'CASE  ' +
+        '    WHEN path LIKE "accommodation.%" THEN 0.8 ' +
+        '    WHEN path LIKE "resort.%" THEN 0.9 ' +
+        '    WHEN path LIKE "resortfinder.%" THEN 0.7 ' +
+        '    else 5 ' +
+        'END as priority ' +
+        'FROM routing  ' +
+        'WHERE lang = ? ' +
+        'AND active = 1 ' +
+        'AND redirect = 0 ' +
+        'ORDER BY url ';
 
 
-	dbConProd1.query(queryStr, [options.lang], function(err, rows, fields) {
-		if(err) deferred.reject(err);
+    dbConProd1.query(queryStr, [options.lang], function(err, rows, fields) {
+        if (err) deferred.reject(err);
 
-		// if(rows[0].url.indexOf('//') != -1){ //bug fix, some (hr, sp ...) domains have root
-		// 	rows.splice(0, 0);
-		// }
-		// rows.unshift({url:'/'+options.lang+'/', priority: 1, freq: 'daily'}); //append domain root
-		options.data = rows;
-		options.fields = fields;
-		options.dist = 'dist';
+        // if(rows[0].url.indexOf('//') != -1){ //bug fix, some (hr, sp ...) domains have root
+        // 	rows.splice(0, 0);
+        // }
+        // rows.unshift({url:'/'+options.lang+'/', priority: 1, freq: 'daily'}); //append domain root
+        options.data = rows;
+        options.fields = fields;
+        options.dist = 'dist';
 
-		deferred.resolve(options);
-	});
+        deferred.resolve(options);
+    });
 
-	return deferred.promise;
+    return deferred.promise;
 };
 
-_.each(domains, function( attrs, domain ) {
+_.each(domains, function(attrs, domain) {
 
-	attrs.domain = domain;
+    attrs.domain = domain;
 
-	var domainQ = getUrls( attrs )
-		.then( sm.mkDistDir )
-		.then( sm.generateXml )
-		// .then( sm.saveToFile )
-		.then( sm.gzip )
-		.then( sm.saveToFile );
+    var domainQ = getUrls(attrs)
+        .then(sm.mkDistDir)
+        .then(sm.generateXml)
+        // .then( sm.saveToFile )
+        .then(sm.gzip)
+        .then(sm.saveToFile);
 
-	domainsQ.push(domainQ);
+    domainsQ.push(domainQ);
 
 });
 
 // Q.allSettled(domainsQ)
 Q.all(domainsQ)
-	.then(function (results, err) {
+    .then(function(results, err) {
 
-		if (err) {
-			console.error(err);
-			process.exit(code=1);
-		} else {
-			console.info('All sitemaps build-ed successfully!');
-			process.exit(code=0);
+        if (err) {
+            console.error(err);
+            process.exit(code = 1);
+        } else {
+            console.info('All sitemaps build-ed successfully!');
+            process.exit(code = 0);
 
-		}
+        }
 
-	})
-	.fail( function(err) {
-		console.error(err);
-		process.exit(code=1);
-	});
+    })
+    .fail(function(err) {
+        console.error(err);
+        process.exit(code = 1);
+    });
